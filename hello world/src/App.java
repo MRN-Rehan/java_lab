815efdb5917437cbef1cf8ea3de24d1fb4851ca4
MRN-Rehan <mdrehannalband@gmail.com>
2026-04-30 21:10:14 +0530

    wtf no scanner?

--- hello world/src/App.java
+++ hello world/src/App.java
@@ -1,7 +1,10 @@
+import java.util.spi.Scanner;
+
 public class App 
 {
     public static void main(String[] args) throws Exception
     {
+        Scanner sc = new java.util.Scanner(System.in );
         System.out.println("Hello, World!");
     }
 }
