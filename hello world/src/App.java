35eb4f9a4bb8b71b6c8bf55215870336f8fd24cc
MRN-Rehan <mdrehannalband@gmail.com>
2026-05-01 23:00:13 +0530

    input/output

--- hello world/src/App.java
+++ hello world/src/App.java
@@ -1,10 +1,14 @@
-import java.util.spi.Scanner;
+import java.util.Scanner;
 
 public class App 
 {
     public static void main(String[] args) throws Exception
     {
         Scanner sc = new java.util.Scanner(System.in );
-        System.out.println("Hello, World!");
+        System.out.println("hiii whats your name");
+        String name = sc.nextLine ();
+        System.out.println("Hello, World!" +name);
+
+        sc.close();
     }
 }
